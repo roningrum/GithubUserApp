9afba95a19a3f7b2dd28a606e5cc43d70c50859b
ochasamu <roningrum891@gmail.com>
2020-10-29 07:05:21 +0700

    update list to recyclerview

--- app/src/main/java/com/roningrum/githubuserapp/MainActivity.kt
+++ app/src/main/java/com/roningrum/githubuserapp/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.roningrum.githubuserapp
 
+import android.content.Intent
 import android.content.res.TypedArray
 import android.os.Bundle
-import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.roningrum.githubuserapp.adapter.GithubAdapter
 import com.roningrum.githubuserapp.data.Users
 
@@ -11,6 +13,10 @@ class MainActivity : AppCompatActivity() {
     private lateinit var adapter: GithubAdapter
     private lateinit var dataUsername: Array<String>
     private lateinit var dataName: Array<String>
+    private lateinit var dataCompany: Array<String>
+    private lateinit var dataRepository: IntArray
+    private lateinit var dataFollower: IntArray
+    private lateinit var dataFollowing: IntArray
     private lateinit var dataLocation: Array<String>
     private lateinit var dataAvatar: TypedArray
 
@@ -19,12 +25,25 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val listView: ListView = findViewById(R.id.lv_github_users)
-        adapter = GithubAdapter(this)
-        listView.adapter = adapter
+        val rvListUsers: RecyclerView = findViewById(R.id.rv_github_users)
+        rvListUsers.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        adapter = GithubAdapter(githubUsers)
+        rvListUsers.adapter = adapter
+        adapter.setOnItemClickCallback(object : GithubAdapter.OnItemClickCallback {
+            override fun onItemClick(user: Users) {
+                val intent = Intent(applicationContext, DetailActivity::class.java)
+                intent.putExtra(DetailActivity.EXTRA_USERS, user)
+                startActivity(intent)
+            }
 
+        })
+//        adapter = GithubAdapter(this)
+//        listView.adapter = adapter
         prepareData()
         addItemToList()
+//        listView.onItemClickListener = AdapterView.OnItemClickListener { adapterView, view, i, l ->
+
+//        }
     }
 
     private fun addItemToList() {
@@ -33,11 +52,15 @@ class MainActivity : AppCompatActivity() {
                 dataName[position],
                 dataUsername[position],
                 dataLocation[position],
+                dataRepository[position],
+                dataCompany[position],
+                dataFollower[position],
+                dataFollowing[position],
                 dataAvatar.getResourceId(position, -1)
             )
             githubUsers.add(users)
         }
-        adapter.users = githubUsers
+
     }
 
     private fun prepareData() {
@@ -45,5 +68,10 @@ class MainActivity : AppCompatActivity() {
         dataUsername = resources.getStringArray(R.array.username)
         dataLocation = resources.getStringArray(R.array.location)
         dataAvatar = resources.obtainTypedArray(R.array.avatar)
+        dataRepository = resources.getIntArray(R.array.repository)
+        dataFollower = resources.getIntArray(R.array.followers)
+        dataFollowing = resources.getIntArray(R.array.following)
+        dataRepository = resources.getIntArray(R.array.repository)
+        dataCompany = resources.getStringArray(R.array.company)
     }
 }
--- app/src/main/java/com/roningrum/githubuserapp/adapter/GithubAdapter.kt
+++ app/src/main/java/com/roningrum/githubuserapp/adapter/GithubAdapter.kt
@@ -1,37 +1,73 @@
 package com.roningrum.githubuserapp.adapter
 
-import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.BaseAdapter
 import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
 import com.roningrum.githubuserapp.R
 import com.roningrum.githubuserapp.data.Users
 import de.hdodenhof.circleimageview.CircleImageView
 
-class GithubAdapter internal constructor(private val context: Context) : BaseAdapter() {
-    internal var users = arrayListOf<Users>()
 
-    override fun getCount(): Int = users.size
+//listview
+//class GithubAdapter internal constructor(private val context: Context) : BaseAdapter() {
+//    internal var users = arrayListOf<Users>()
+//
+//    override fun getCount(): Int = users.size
+//
+//    override fun getItem(i: Int): Any = users[i]
+//
+//    override fun getItemId(i: Int): Long = i.toLong()
+//
+//    override fun getView(position: Int, view: View?, viewGroup: ViewGroup): View {
+//        var itemView = view
+//        if (itemView == null) {
+//            itemView =
+//                LayoutInflater.from(context).inflate(R.layout.item_github_user, viewGroup, false)
+//        }
+//        val viewHolder = ViewHolder(itemView as View)
+//        val users = getItem(position) as Users
+//        viewHolder.bind(users)
+//        return itemView
+//    }
+//
+//    class ViewHolder internal constructor(view: View) {
 
-    override fun getItem(i: Int): Any = users[i]
+//
+//        internal fun bind(user: Users) {
+//            txtName.text = user.name
+//            txtUserName.text = user.username
+//            txtLocation.text = user.location
+//            imgPhoto.setImageResource(user.avatar)
+//        }
+//    }
+//}
+class GithubAdapter(private val users: List<Users>) :
+    RecyclerView.Adapter<GithubAdapter.ItemViewHolder>() {
+    private lateinit var onItemCallbacks: OnItemClickCallback
 
-    override fun getItemId(i: Int): Long = i.toLong()
-
-    override fun getView(position: Int, view: View?, viewGroup: ViewGroup): View {
-        var itemView = view
-        if (itemView == null) {
-            itemView =
-                LayoutInflater.from(context).inflate(R.layout.item_github_user, viewGroup, false)
-        }
-        val viewHolder = ViewHolder(itemView as View)
-        val users = getItem(position) as Users
-        viewHolder.bind(users)
-        return itemView
+    fun setOnItemClickCallback(onItemClickCallback: OnItemClickCallback) {
+        this.onItemCallbacks = onItemClickCallback
     }
 
-    class ViewHolder internal constructor(view: View) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder =
+        ItemViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.item_github_user, parent, false)
+        )
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        holder.bind(users[position])
+        holder.itemView.setOnClickListener {
+            onItemCallbacks.onItemClick(users[holder.adapterPosition])
+        }
+    }
+
+    override fun getItemCount() = users.size
+
+
+    class ItemViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
         private val txtName: TextView = view.findViewById(R.id.txt_user_name_profile)
         private val imgPhoto: CircleImageView = view.findViewById(R.id.img_user_photo)
         private val txtUserName: TextView = view.findViewById(R.id.txt_username)
@@ -44,6 +80,12 @@ class GithubAdapter internal constructor(private val context: Context) : BaseAda
             imgPhoto.setImageResource(user.avatar)
         }
     }
+
+    interface OnItemClickCallback {
+        fun onItemClick(user: Users)
+    }
 }
+
+
 
 
